17b8d0819d17667cb1f15e8ce03d6c885bfa0b8d
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-11-04 16:02:17 +0100

    a dynamic output

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Player from "./components/Player";
 
 const App = () => {
   return (
     <>
-      
+      <Header />
+      <main>
+        <div id="game-container">
+          <ol id="players">
+           <Player name="Player 1" symbol="X" />
+           <Player name="Player 2" symbol="0" />
+          </ol>
+          GameBoard
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

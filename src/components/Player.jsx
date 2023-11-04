d09fc80cd8add2d3de02a2b2c12e24511e104038
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-11-04 16:01:52 +0100

    two-way-binding the component for the input button

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const Player = (props) => {
+
+    const [playerName,setPlayerName] = useState(props.name)
+    const [isEditing, setIsEditing] = useState(false);
+
+    const HandleIsEditing = () => {
+        setIsEditing((editing) => !editing);
+    }
+
+    const ChangePlayerNameHandler = (event) => {
+        setPlayerName(event.target.value)
+
+    }
+
+  return (
+    <li>
+      <span className="player">
+        { isEditing ? <input type="text" required value={playerName} onChange={ChangePlayerNameHandler}/> :<span className="player-name">{playerName}</span>}
+        <span className="player-x">{props.symbol}</span>
+      </span>
+      <button onClick={HandleIsEditing}>{isEditing ?"save"  : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;

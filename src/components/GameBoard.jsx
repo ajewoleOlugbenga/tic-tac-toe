ac14b9aa83b3c96333ae86f06d74139b793d58c9
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-11-06 10:07:54 +0100

    updating the row and cell base on the player's click

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-
 const initialGame = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
-const GameBoard = () => {
+const GameBoard = ({ onSelectSquare, turns }) => {
+  let gameBoard = initialGame;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, cell } = square;
+
+    gameBoard[row][cell] = player;
+  }
+
+  // const [gameBoard,setGameBoard] = useState(initialGame);
+
+  // const HandleSelectedSquare = (rowIndex,cellIndex) => {
+  //     setGameBoard((prevGameBoard => {
+  //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //         updatedBoard[rowIndex][cellIndex] = props.activeSymbol;
+
+  //         return updatedBoard;
+  //     }))
+
+  //     props.onSelectSquare();
+  // }
+
   return (
-    <ol className='game-board'>
-        {initialGame.map((row, rowIndex) => (<li key={rowIndex}>
-            <ol>
-                {row.map((playerSymbol,cellIndex) => <li key={cellIndex}>
-                    <button>{playerSymbol}</button>
-                </li>)}
-
-            </ol>
-        </li>)) 
-}
+    <ol id="game-board">
+     {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, cellIndex) => (
+              <li key={cellIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, cellIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
-  )
-}
+  );
+};
 
-export default GameBoard
+export default GameBoard;
